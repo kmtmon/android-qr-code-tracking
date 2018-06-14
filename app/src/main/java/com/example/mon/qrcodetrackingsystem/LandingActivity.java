66a40c19b460e024ab0b8f8c6961e86df8dd404b
Mon <mon@sorealprop.com>
2018-06-14 18:01:46 +0800

    Integrate firebase sdk, added landing activity and make it as launcher

---
+++ app/src/main/java/com/example/mon/qrcodetrackingsystem/LandingActivity.java
@@ -0,0 +1,16 @@
+package com.example.mon.qrcodetrackingsystem;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LandingActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landing);
+
+
+
+    }
+}
